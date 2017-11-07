c2c44d7b6b4d780a58daf00dbafd40a9a2a77d7d
Adrian Pastur <adrian.pastur@sage.com>
2017-11-07 09:19:27 +0000

    First commit

---
+++ spec/bowling_spec.rb
@@ -0,0 +1,18 @@
+describe 'players'
+  it 'can have multiple players'
+  it 'have different names'
+end
+
+describe 'score'
+  it 'one instance for every player'
+  it 'calculates total score'
+  it 'handles spares'
+  it 'handles strikes'
+end
+
+describe 'user input'
+  it 'can be modified'
+  it 'takes user input for every line'
+  it 'is restricted to 10 turns'
+  it 'has two triest per turn'
+end
